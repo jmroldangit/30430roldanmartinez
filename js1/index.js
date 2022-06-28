c7fa3aaec9408896d563dd4228051def625b5601
Juan Roldan <juanroldanm13011301@gmail.com>
2022-06-28 18:38:47 -0300

    inicio

---
+++ js1/index.js
@@ -0,0 +1,22 @@
+/* comentario en bloque se ingresa con ctrl+shift+A */
+let nombre = "Juan"; /* esta variable reserva un espacio para asignar el nombre en este caso. las comillas son para cadenas de texto. No se usan para los números o variables  */
+const edad = 15;/* Para reservar constantes . No va con comillas porque es una variable o number. Con comillas simples es un string*/
+var apoellido = "Ruiz";// no es recomendable usar
+console.log(nombre);
+console.log(typeof(edad));
+const PREMIOS = "5";// Son constantes. No cambian.
+
+prompt(ingrese su nombre);
+ 
+let edad = 18;
+let permiso = > 18;
+
+if(premiso){
+    console.log("Puedes entrar")
+}else{
+    console.log("No puedes entrar");
+}
+
+)
+
+
