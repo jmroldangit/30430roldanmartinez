166e3541c23689f367c0b5310984b988c364f9d1
Juan Roldan <juanroldanm13011301@gmail.com>
2022-07-14 22:42:30 -0300

    primer entrega

--- js1/index.js
+++ js1/index.js
@@ -3,15 +3,15 @@ function cotizadorSuscripcion(){
   let servicioSuscripcion= prompt("Seleccione el tipo de suscripción, semanal escriba S, mensual escriba M").toLowerCase();
   let valorMensual=1500;
   let valorSemanal=1800;
-  if (servicioSuscripcion == "S" || servicioSuscripcion== "M"){
+  if (servicioSuscripcion == "s" || servicioSuscripcion== "m"){
       let mesesSuscripcion = Number  (prompt("por cuántos meses desea recibir novedades?"));
   
-          if (servicioSuscripcion =="M"){
+          if (servicioSuscripcion =="m"){
               
           let resultado=  (valorMensual * mesesSuscripcion);
           alert(`El valor de la suscripción por ${mesesSuscripcion} mes(es) es: ${resultado}$`);
           }
-          else if(servicioSuscripcion == "S"){
+          else if(servicioSuscripcion == "s"){
               let resultado=  (valorSemanal * mesesSuscripcion);
               alert(`El valor de la suscripción al boletín semanal por ${mesesSuscripcion} mes(es) es: ${resultado}$`);;
           }
@@ -21,3 +21,31 @@ function cotizadorSuscripcion(){
           }
   }
   cotizadorSuscripcion();
+
+  class Suscripcion{
+    constructor(nombre, precio){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.vendido = false;
+  }
+  vender(){
+    this.vendido = true;
+  }
+}
+let productos = [];
+
+productos.push(new suscripcion("mensual", 1500));
+productos.push(new suscripcion("semanal", 1800));
+
+
+for(const item of productos){
+    if(item.nombre === "mensual"){
+        item.vender()
+    }
+    else if(item.nombre === "semanal"){
+        item.vender()
+    }
+    
+  }
+
+console.log(productos);
